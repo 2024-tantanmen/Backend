ffaf2adea68d52fdc3bbdf125015e5fd9897bc2e
junhaa <2171326@hansung.ac.kr>
2024-07-17 00:12:41 +0900

    :tada: setup

---
+++ src/main/java/com/tantanmen/carbofootprint/CarboFootprintApplication.java
@@ -0,0 +1,13 @@
+package com.tantanmen.carbofootprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarboFootprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarboFootprintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tantanmen/carbofootprint/CarboFootprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tantanmen.carbofootprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarboFootprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

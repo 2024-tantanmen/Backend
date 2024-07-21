4deeb15869512cde36c5a77b1054adab72665f1f
junhaa <2171326@hansung.ac.kr>
2024-07-22 05:55:02 +0900

    :sparkles: FEAT. 음식 추천 관련 예외 추가

--- src/main/java/com/tantanmen/carbofootprint/global/enums/statuscode/ErrorStatus.java
+++ src/main/java/com/tantanmen/carbofootprint/global/enums/statuscode/ErrorStatus.java
@@ -7,10 +7,19 @@ public enum ErrorStatus implements BaseCode {
 	_INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러, 관리자에게 문의 바랍니다."),
 	_BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON400", "잘못된 요청입니다."),
 	_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401", "인증이 필요합니다."),
-	_FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다.");
+	_FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
+
+
+	// recommend
+	_ALLERGEN_EMPTY_ERROR(HttpStatus.BAD_REQUEST, "RECOMMEND4001", "선택된 알레르기가 존재하지 않습니다."),
+	_PREFERENCE_EMPTY_ERROR(HttpStatus.BAD_REQUEST, "RECOMMEND4002", "선택된 식습관이 존재하지 않습니다.");
+
+
 	private final HttpStatus httpStatus;
 	private final String code;
 	private final String message;
+
+
 
 	ErrorStatus(HttpStatus httpStatus, String code, String message) {
 		this.httpStatus = httpStatus;

68297ba1b9894f7e2882b97375af41e4597e6a1e
= <=>
2024-07-25 02:46:51 +0900

    :art: ADD. Global 설정

--- src/main/java/com/tantanmen/carbofootprint/CarboFootprintApplication.java
+++ src/main/java/com/tantanmen/carbofootprint/CarboFootprintApplication.java
@@ -3,9 +3,11 @@ package com.tantanmen.carbofootprint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableJpaRepositories
 public class CarboFootprintApplication {
 
 	public static void main(String[] args) {

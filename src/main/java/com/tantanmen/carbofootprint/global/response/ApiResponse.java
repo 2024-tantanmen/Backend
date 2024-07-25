a195707cfddca86fceba5fc45ee9e91e3c6ed2fb
junhaa <2171326@hansung.ac.kr>
2024-07-25 15:06:22 +0900

    :memo: DOCS. ApiReponse swagger Schema 명세 작성

--- src/main/java/com/tantanmen/carbofootprint/global/response/ApiResponse.java
+++ src/main/java/com/tantanmen/carbofootprint/global/response/ApiResponse.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.tantanmen.carbofootprint.global.enums.statuscode.BaseCode;
 import com.tantanmen.carbofootprint.global.enums.statuscode.SuccessStatus;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -11,11 +12,13 @@ import lombok.Getter;
 @AllArgsConstructor
 @JsonPropertyOrder({"isSuccess", "code", "message", "result"})
 public class ApiResponse<T> {
-
+	@Schema(description = "성공 여부", example = "true")
 	private final Boolean isSuccess;
+	@Schema(description = "응답 코드", example = "USER4001")
 	private final String code;
-
+	@Schema(description = "응답 메세지", example = "성공입니다.")
 	private final String message;
+	@Schema(description = "응답 데이터")
 	private T result;
 
 	// 성공한 경우 응답 생성

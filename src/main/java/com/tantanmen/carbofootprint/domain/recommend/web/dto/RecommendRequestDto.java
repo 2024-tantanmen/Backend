13f81daa0eb40192b88b0e631f56885c64c360db
junhaa <2171326@hansung.ac.kr>
2024-07-25 15:06:49 +0900

    :memo: DOCS. 음식 추천 기능 관련 명세 작성

--- src/main/java/com/tantanmen/carbofootprint/domain/recommend/web/dto/RecommendRequestDto.java
+++ src/main/java/com/tantanmen/carbofootprint/domain/recommend/web/dto/RecommendRequestDto.java
@@ -1,11 +1,11 @@
 package com.tantanmen.carbofootprint.domain.recommend.web.dto;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.tantanmen.carbofootprint.domain.recommend.enums.AllergenType;
 import com.tantanmen.carbofootprint.domain.recommend.enums.PreferenceType;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -13,12 +13,16 @@ import lombok.ToString;
 /**
  * 음식 추천 요청 Dto
  */
+
+@Schema(title = "RECOMMEND_REQ_01", description = "음식 추천 요청 DTO")
 public class RecommendRequestDto {
 	@Setter
 	@Getter
 	@ToString
-	public static class RecommendFoodRequestDto{
+	public static class RecommendFoodRequestDto {
+		@Schema(description = "알레르기 목록", example = "[\"난류\", \"고등어\"]")
 		private List<AllergenType> allergen_list;
+		@Schema(description = "식습관 목록", example = "[\"아재_입맛\", \"다이어트\"]")
 		private List<PreferenceType> preference_list;
 	}
 
--- src/main/java/com/tantanmen/carbofootprint/domain/recommend/web/dto/RecommendResponseDto.java
+++ src/main/java/com/tantanmen/carbofootprint/domain/recommend/web/dto/RecommendResponseDto.java
@@ -2,6 +2,7 @@ package com.tantanmen.carbofootprint.domain.recommend.web.dto;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,9 +17,12 @@ public class RecommendResponseDto {
 	@Getter
 	@NoArgsConstructor
 	@AllArgsConstructor
-	public static class RecommendFoodResponseDto{
+	public static class RecommendFoodResponseDto {
+		@Schema(description = "추천 음식 목록")
 		List<RecommendFoodDetailDto> food_list;
+		@Schema(description = "사용자가 입력한 알레르기 목록", example = "[\"난류\", \"고등어\"]")
 		List<String> allergen_list;
+		@Schema(description = "사용자가 입력한 식습관 목록", example = "[\"아재_입맛\", \"다이어트\"]")
 		List<String> preference_list;
 	}
 
@@ -26,13 +30,17 @@ public class RecommendResponseDto {
 	@Getter
 	@NoArgsConstructor
 	@AllArgsConstructor
-	public static class RecommendFoodDetailDto{
+	public static class RecommendFoodDetailDto {
+		@Schema(description = "음식 이름", example = "삼겹살")
 		private String name;
+		@Schema(description = "음식 이미지 URL 주소", example = "https://carbofootprint-bucket.s3.ap-northeast-2.amazonaws.com/foods/images/%EC%82%BC%EA%B2%B9%EC%82%B4.jpg")
 		private String image_url;
+		@Schema(description = "열량", example = "660")
 		private Integer calorie;
+		@Schema(description = "탄수화물", example = "0")
 		private Integer carbohydrate;
+		@Schema(description = "당류", example = "0")
 		private Integer saccharide;
 	}
-
 
 }

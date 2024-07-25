b423dcc595fbcbc953a39055061a8a5322f02a44
junhaa <2171326@hansung.ac.kr>
2024-07-25 15:06:41 +0900

    :memo: DOCS. 음식 사진 인식 AI 기능 관련 명세 작성

--- src/main/java/com/tantanmen/carbofootprint/domain/classification/web/controller/ClassificationRestController.java
+++ src/main/java/com/tantanmen/carbofootprint/domain/classification/web/controller/ClassificationRestController.java
@@ -1,10 +1,6 @@
 package com.tantanmen.carbofootprint.domain.classification.web.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +13,16 @@ import com.tantanmen.carbofootprint.domain.classification.web.dto.Classification
 import com.tantanmen.carbofootprint.domain.classification.web.dto.ClassificationResponseDto;
 import com.tantanmen.carbofootprint.global.response.ApiResponse;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import jakarta.validation.ValidationException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+@Tag(name = "음식 사진 성분 인식 API", description = "사진 내부의 음식 인식 및 성분 분석 API")
 @Slf4j
 @RestController
 @RequestMapping("/api/classification")
@@ -32,9 +33,46 @@ public class ClassificationRestController {
 	/**
 	 * 음식 사진 분석 AI
 	 */
-	@PostMapping("")
-	public ApiResponse<ClassificationResponseDto.FoodClassificationResponseDto> foodClassification(@Valid @ModelAttribute
-		ClassificationRequestDto.FoodClassificationRequestDto request){
+	@Operation(summary = "음식 사진 분석 API 요청", description = "사진 내부의 음식 인식 및 성분 분석 API 요청")
+	@ApiResponses(value = {
+		@io.swagger.v3.oas.annotations.responses.ApiResponse(
+			responseCode = "COMMON200",
+			description = "요청 성공",
+			content = {
+				@Content(
+					schema = @Schema(
+						implementation = ClassificationResponseDto.FoodClassificationResponseDto.class
+					)
+				)
+			}
+		),
+		@io.swagger.v3.oas.annotations.responses.ApiResponse(
+			responseCode = "CLASSIFICATION5001",
+			description = "음식 사진 AI 서버 내부 오류 발생",
+			content = {
+				@Content(
+					schema = @Schema(
+						example = "음식 사진 AI 요청 중 오류가 발생했습니다."
+					)
+				)
+			}
+		),
+		@io.swagger.v3.oas.annotations.responses.ApiResponse(
+			responseCode = "CLASSIFICATION4001",
+			description = "인식할 수 없는 음식 사진인 경우 (여러개의 음식이 같이 있거나, 학습되지 않은 사진인 경우)",
+			content = {
+				@Content(
+					schema = @Schema(
+						example = "음식 사진 인식에 실패했습니다."
+					)
+				)
+			}
+		)
+	})
+	@PostMapping(value = "", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ApiResponse<ClassificationResponseDto.FoodClassificationResponseDto> foodClassification(
+		@Valid @ModelAttribute
+		ClassificationRequestDto.FoodClassificationRequestDto request) {
 
 		Food food = classificationService.foodClassification(request);
 

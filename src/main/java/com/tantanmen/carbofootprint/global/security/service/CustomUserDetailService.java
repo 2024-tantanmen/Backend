785885d081904dad3f6d7f1d98f7e30c13ea2eb1
= <=>
2024-07-27 12:57:58 +0900

    :sparkles: FEAT. CustomUserDetails, CustomUserDetailService 작성

---
+++ src/main/java/com/tantanmen/carbofootprint/global/security/service/CustomUserDetailService.java
@@ -0,0 +1,33 @@
+package com.tantanmen.carbofootprint.global.security.service;
+
+import com.tantanmen.carbofootprint.domain.user.entity.User;
+import com.tantanmen.carbofootprint.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
+        User user = userRepository.findByLoginId(loginId).orElseThrow(RuntimeException::new);
+
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getRole().name()))
+                .collect(Collectors.toList());
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getLoginId(), user.getPassword(), authorities
+        );
+    }
+}

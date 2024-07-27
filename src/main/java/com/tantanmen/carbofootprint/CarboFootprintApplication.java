f0ccc68fb70e6ce183349f2a88a6ecd520d9eb70
= <=>
2024-07-27 12:54:45 +0900

    :heavy_plus_sign: ADD. Security Dependency 추가

--- src/main/java/com/tantanmen/carbofootprint/CarboFootprintApplication.java
+++ src/main/java/com/tantanmen/carbofootprint/CarboFootprintApplication.java
@@ -2,12 +2,14 @@ package com.tantanmen.carbofootprint;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableCaching
 public class CarboFootprintApplication {
 
 	public static void main(String[] args) {

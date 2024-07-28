9a5a3e70bd1d5e3b791c5e2cb7951ddc114bcfac
junhaa <2171326@hansung.ac.kr>
2024-07-28 16:05:40 +0900

    :sparkles: FEAT. LoginMemberArgumentResolver 로그인 된 사용자 Argument Resolver 구현

--- src/main/java/com/tantanmen/carbofootprint/global/config/WebConfig.java
+++ src/main/java/com/tantanmen/carbofootprint/global/config/WebConfig.java
@@ -1,11 +1,21 @@
 package com.tantanmen.carbofootprint.global.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.tantanmen.carbofootprint.domain.member.repository.MemberRepository;
+import com.tantanmen.carbofootprint.global.resolver.LoginMemberArgumentResolver;
+
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+    private final MemberRepository memberRepository;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -14,5 +24,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver(memberRepository));
     }
 }

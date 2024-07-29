82ed509530b2f1cd52d0ce4a30f52342b488893d
junhaa <2171326@hansung.ac.kr>
2024-07-29 10:52:41 +0900

    :sparkles: FEAT. WebSocket 연결 시 사용자 정보 세션 저장 기능 구현

--- src/main/java/com/tantanmen/carbofootprint/global/socket/config/WebSocketConfig.java
+++ src/main/java/com/tantanmen/carbofootprint/global/socket/config/WebSocketConfig.java
@@ -6,12 +6,19 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import com.tantanmen.carbofootprint.global.security.jwt.JwtTokenProvider;
+import com.tantanmen.carbofootprint.global.socket.interceptor.JwtHandshakeInterceptor;
+
+import lombok.RequiredArgsConstructor;
+
 /**
  * Web Socket config
  */
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+	private final JwtTokenProvider jwtTokenProvider;
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -21,7 +28,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/ws").withSockJS(); // ws://[URI]/ws로 웹소켓 연결
+		registry.addEndpoint("/ws")
+			.setAllowedOriginPatterns("*")
+			.addInterceptors(new JwtHandshakeInterceptor(jwtTokenProvider))
+			.withSockJS(); // http://[URI]/ws로 웹소켓 연결, SockJS가 아닌 경우 ws://[URI]/ws
 	}
 
 }

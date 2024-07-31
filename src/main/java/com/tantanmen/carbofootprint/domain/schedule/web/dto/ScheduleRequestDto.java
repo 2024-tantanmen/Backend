01ad084037a2b959962a4dd84cdd470476b167df
junhaa <2171326@hansung.ac.kr>
2024-07-31 15:13:51 +0900

    :sparkles: FEAT. 탄수 발자국 날짜 유효성 검증 추가

--- src/main/java/com/tantanmen/carbofootprint/domain/schedule/web/dto/ScheduleRequestDto.java
+++ src/main/java/com/tantanmen/carbofootprint/domain/schedule/web/dto/ScheduleRequestDto.java
@@ -6,7 +6,10 @@ import org.hibernate.validator.constraints.Length;
 
 import com.tantanmen.carbofootprint.domain.schedule.enums.FoodType;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -22,7 +25,14 @@ public class ScheduleRequestDto {
 		@Length(max = 10, message = "일정 이름은 최대 10글자 입니다.")
 		@NotBlank(message = "일정 이름을 작성해주세요.")
 		private String name;
+		@NotNull(message = "날짜를 입력해주세요.")
+		@Min(value = 8, message = "8월 이상의 데이터만 요청 가능합니다.")
+		@Max(value = 9, message = "최대 9월까지의 데이터만 요청 가능합니다.")
 		private Long month;
+
+		@NotNull(message = "날짜를 입력해주세요.")
+		@Min(value = 1, message = "날짜 입력이 잘못되었습니다.")
+		@Max(value = 31, message = "날짜 입력이 잘못되었습니다.")
 		private Long day;
 		private List<FoodType> first_meal;
 		private List<FoodType> second_meal;

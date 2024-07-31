c85530e16f80913a6264ed684b775b27f8ffb7bb
junhaa <2171326@hansung.ac.kr>
2024-07-31 19:11:29 +0900

    :memo: DOCS. 사용자 관련 swagger 명세 작성

--- src/main/java/com/tantanmen/carbofootprint/domain/member/web/controller/MemberController.java
+++ src/main/java/com/tantanmen/carbofootprint/domain/member/web/controller/MemberController.java
@@ -7,22 +7,44 @@ import com.tantanmen.carbofootprint.domain.member.web.dto.LoginRequestDto;
 import com.tantanmen.carbofootprint.domain.member.web.dto.LoginResponseDto;
 import com.tantanmen.carbofootprint.domain.member.web.dto.SignUpResponseDto;
 import com.tantanmen.carbofootprint.domain.member.web.dto.SignUpRequestDto;
+import com.tantanmen.carbofootprint.domain.recommend.web.dto.RecommendResponseDto;
 import com.tantanmen.carbofootprint.global.annotation.LoginMember;
 import com.tantanmen.carbofootprint.global.response.ApiResponse;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Tag(name = "사용자 관련 API", description = "사용자 관련 API")
 @Slf4j
+@RestController
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
 
     // SignUp
+    @Operation(summary = "회원가입 API", description = "회원가입 API")
+    @ApiResponses(value = {
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(
+            responseCode = "COMMON200",
+            description = "요청 성공",
+            content = {
+                @Content(
+                    schema = @Schema(
+                        implementation = SignUpResponseDto.class
+                    )
+                )
+            }
+        )
+    })
     @PostMapping("/signup")
     public ApiResponse<SignUpResponseDto> signUp(@Valid @RequestBody SignUpRequestDto.Request request) {
 
@@ -36,6 +58,20 @@ public class MemberController {
     }
 
     // Login
+    @Operation(summary = "로그인 API", description = "로그인 API")
+    @ApiResponses(value = {
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(
+            responseCode = "COMMON200",
+            description = "요청 성공",
+            content = {
+                @Content(
+                    schema = @Schema(
+                        implementation = LoginResponseDto.class
+                    )
+                )
+            }
+        )
+    })
     @PostMapping("/login")
     public ApiResponse<LoginResponseDto> login(@RequestBody LoginRequestDto.Request request){
         String token = memberService.login(request);
@@ -47,8 +83,22 @@ public class MemberController {
     }
 
     //attendance
+    @Operation(summary = "연속 출석 일수 요청 API", description = "연속 출석 일수 요청 API")
+    @ApiResponses(value = {
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(
+            responseCode = "COMMON200",
+            description = "요청 성공",
+            content = {
+                @Content(
+                    schema = @Schema(
+                        implementation = AttendanceResponseDto.GetAttendanceResponseDto.class
+                    )
+                )
+            }
+        )
+    })
     @GetMapping("/attendance")
-    public ApiResponse<AttendanceResponseDto.GetAttendanceResponseDto> getAttendance(@LoginMember Member member){
+    public ApiResponse<AttendanceResponseDto.GetAttendanceResponseDto> getAttendance(@Parameter(hidden = true) @LoginMember Member member){
         AttendanceResponseDto.GetAttendanceResponseDto result = AttendanceResponseDto.GetAttendanceResponseDto.builder()
             .attendance_streak_count(member.getAttendanceStreakCount())
             .build();
--- src/main/java/com/tantanmen/carbofootprint/domain/member/web/dto/AttendanceResponseDto.java
+++ src/main/java/com/tantanmen/carbofootprint/domain/member/web/dto/AttendanceResponseDto.java
@@ -1,11 +1,12 @@
 package com.tantanmen.carbofootprint.domain.member.web.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-
+@Schema(title = "ATTENDANCE_RES_01", description = "연속 출석 요청 DTO")
 public class AttendanceResponseDto {
 	/**
 	 * 연속 출석 일수 응답 DTO
@@ -15,6 +16,7 @@ public class AttendanceResponseDto {
 	@NoArgsConstructor
 	@AllArgsConstructor
 	public static class GetAttendanceResponseDto{
+		@Schema(description = "연속 출석 일수", example = "3")
 		private Integer attendance_streak_count;
 	}
 }

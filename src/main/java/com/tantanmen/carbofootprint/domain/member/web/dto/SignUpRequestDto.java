daab5187f8bb6e6bd421f52ec2996475218f36d5
junhaa <2171326@hansung.ac.kr>
2024-08-05 10:54:50 +0900

    :sparkles: FEAT. 회원가입, 비밀번호 유효성 검증 임시 제거

--- src/main/java/com/tantanmen/carbofootprint/domain/member/web/dto/SignUpRequestDto.java
+++ src/main/java/com/tantanmen/carbofootprint/domain/member/web/dto/SignUpRequestDto.java
@@ -17,12 +17,12 @@ public class SignUpRequestDto {
     public static class Request {
 
         @Schema(description = "회원가입 할 로그인 ID", example = "user1234")
-        @Pattern(regexp = "^[a-zA-Z0-9]{5,15}$", message = "아이디는 영문 또는 숫자로 5~15자여야 합니다.")
+        // @Pattern(regexp = "^[a-zA-Z0-9]{5,15}$", message = "아이디는 영문 또는 숫자로 5~15자여야 합니다.")
         @NotEmpty(message = "아이디가 필요합니다.")
         private String loginId;
 
         @Schema(description = "회원가입 할 비밀번호", example = "password123")
-        @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "비밀번호는 최소 하나의 문자 및 하나의 숫자, 특수문자를 포함하여 8자 이상이어야 합니다.")
+        // @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "비밀번호는 최소 하나의 문자 및 하나의 숫자, 특수문자를 포함하여 8자 이상이어야 합니다.")
         @NotEmpty(message = "비밀번호가 필요합니다.")
         private String password;
 
